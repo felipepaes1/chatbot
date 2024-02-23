b1dcb2cd98d285022b5da6ffb76182c7ec578050
Felipe Paes <69680487+felipepaes1@users.noreply.github.com>
2024-02-23 14:56:09 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+import openai
+
+app = Flask(__name__)
+
+openai.api_key = ''
+
+@app.route("/")
+
+def index():
+    return render_template("index.html")
+
+@app.route("/api", methods=["POST"])
+
+def api():
+    message = request.json.get("message")
+    completion = openai.ChatCompletion.create(
+    model = "gpt-3.5-turbo",
+
+    messages=[
+        {"role": "system", "content":"Você é um assistente de programação."},
+        {"role": "user", "content": message},
+    ]
+    )
+    
+    if completion.choices[0].message!=None:
+            return completion.choices[0].message
+    
+    else :
+            return 'Falha para geração da resposta!'
+    
+if __name__=='__main__':
+      app.run()
+    
+
